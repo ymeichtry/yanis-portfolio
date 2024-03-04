c792e9666f8e14a8236d93b3bc06fe8964c68df6
CriminalChicken <y.meichtry@gmx.ch>
2024-03-04 16:18:43 +0100

    changed MainPage

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -4,7 +4,10 @@ function MainPage() {
   return (
     <div className="main-page">
       <div className="content">
-        <h1>Welcome to ymeichtry</h1>
+        <h1>I'm Yanis Meichtry</h1>
+        <h1>A Software Programmer</h1>
+        <h1>based in Zürich</h1>
+
         <p>This is the main page content.</p>
       </div>
     </div>
