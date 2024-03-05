520adffd34ac0c0987eddd56da1a6d1195375eed
CriminalChicken <y.meichtry@gmx.ch>
2024-03-05 12:08:17 +0100

    now i can go from a page to another page with Route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import MainPage from './components/MainPage/MainPage';
 import Footer from './components/Footer/Footer';
 import ContactPage from './components/ContactPage/ContactPage';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div>
         <Header />
-        <Switch>
-          <Route path="/" exact component={MainPage} />
-          <Route path="/contact" component={ContactPage} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
