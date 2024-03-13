e7f3fc58e554c1cae0a2ecf221f014d10fa48fef
CriminalChicken <y.meichtry@gmx.ch>
2024-03-13 16:48:45 +0100

    made styling changes in MainPage and added new Image

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
+import './MainPage.css';
+
+import mainImg from "../../images/profile_Image.jpeg";
 
 function MainPage() {
   return (
     <div className="main-page">
-      <div className="content">
-        <h1>I'm Yanis Meichtry</h1>
-        <h1>A Software Programmer</h1>
-        <h1>based in Zürich</h1>
+      <div className="first-section"> 
+        <img src={mainImg} alt="main-Img" />
+        <div className="content">
+          <h1>I'm Yanis Meichtry</h1>
+          <h1>A Software Programmer</h1>
+          <h1>based in Zürich</h1>
 
-        <p>This is the main page content.</p>
+          <p>This is the main page content.</p>
+        </div>
       </div>
     </div>
   );
