7c56ea745a8b3176426fb0534f6649bac25ac6b3
CriminalChicken <y.meichtry@gmx.ch>
2024-03-19 13:53:29 +0100

    made Tech Hobbies boxes/divs

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -33,9 +33,23 @@ function MainPage() {
       </div>
       <div class="my-hobbies">
         <h2>My Interessts</h2>
-        <img src={programmingImg} alt="hobbiesProgrammingImg"/>
-        <img src={musicImg} alt="hobbiesMusicImg"/>
-        <img src={tradingImg} alt="hobbiesTradingImg"/>
+        <div className="hobbies-tech">
+          <div>
+            <img src={programmingImg} alt="hobbiesProgrammingImg" />
+            <h3>Code</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum auctor luctus. Quisque id risus quis orci scelerisque bibendum. Nulla facilisi. Phasellus in mi justo. Vivamus quis hendrerit urna. Cras ultricies mi eu turpis hendrerit, vitae aliquet est tristique. Nulla facilisi. Nullam nec aliquam est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque ultricies diam a sapien facilisis, nec fermentum est vestibulum. Phasellus id odio suscipit, vehicula turpis vitae, eleifend turpis. Mauris non augue ut nunc gravida sollicitudin. Proin convalli</p>
+          </div>
+          <div>
+            <img src={musicImg} alt="hobbiesMusicImg" />
+            <h3>Music</h3>
+            <p></p>
+          </div>
+          <div>
+            <img src={tradingImg} alt="hobbiesTradingImg" />
+            <h3>Trade</h3>
+            <p></p>
+          </div>
+        </div>
       </div>
     </div>
   );
