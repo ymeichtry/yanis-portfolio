6d11c4c759a81a4fba118ed8928af0f8c8d77be3
CriminalChicken <y.meichtry@gmx.ch>
2024-11-02 16:05:54 +0100

    react-router-dom changes

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from "./Pages/Homepage";
+import ProjectsPage from "./Pages/ProjectsPage";
+import Pageload from "./components/pageload/Pageload";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Pageload />;
+  }
+
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
